cb525097eea4815a683bc44f42d8f0d8de3b8f1b
Mark Robert Henderson <henderson.mark@gmail.com>
2020-04-05 00:08:46 -0400

    Update Store.d.ts

--- Store.d.ts
+++ Store.d.ts
@@ -35,7 +35,7 @@ declare module "orbit-db-store" {
          * @param address 
          * @param options 
          */
-        protected constructor (ipfs: IPFS, identity, address: string, options: IStoreOptions);
+        protected constructor (ipfs: IPFS, identity: any, address: string, options: IStoreOptions);
 
         close(): Promise<void>;
         drop(): Promise<void>;
