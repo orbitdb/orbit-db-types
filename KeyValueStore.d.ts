964c2a22fc295a3ba6a087993d644ecd1e851cfc
Christos Panagiotakopoulos <chrispanag@gmail.com>
2021-10-04 14:21:28 +0200

    [fix] KeyValueStore should return string for put/set/del operations

--- KeyValueStore.d.ts
+++ KeyValueStore.d.ts
@@ -4,9 +4,9 @@ declare module "orbit-db-kvstore" {
     export default class KeyValueStore<V> extends Store {
         get(key: string): V;
 
-        put(key: string, value: V, options?: {}): Promise<void>;
-        set(key: string, value: V, options?: {}): Promise<void>;
+        put(key: string, value: V, options?: {}): Promise<string>;
+        set(key: string, value: V, options?: {}): Promise<string>;
         
-        del(key: string, options?: {}): Promise<void>;
+        del(key: string, options?: {}): Promise<string>;
     }
 }
